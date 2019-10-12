7b7673a0ceca799c04bdbaf57549781617815af2
tongdaxia <tong_y_g@163.com>
2019-10-12 11:32:06 +0800

    终于看懂了回溯算法！

--- spring_cloud_api/src/main/java/com/tyg/Cal/BackpackRe.java
+++ spring_cloud_api/src/main/java/com/tyg/Cal/BackpackRe.java
@@ -7,31 +7,31 @@ public class BackpackRe {
 
     // 回溯算法实现。注意：我把输入的变量都定义成了成员变量。
     private int maxW = Integer.MIN_VALUE; // 结果放到maxW中
-    private int[] weight = {2, 2, 4, 6, 3}; // 物品重量
-    private int n = 5; // 物品个数
+    private int[] weight = {1,1,2, 2, 4, 6, 3}; // 物品重量
+    private int n = weight.length; // 物品个数
     private int w = 9; // 背包承受的最大重量
     private ArrayList arr = new ArrayList();
 
     public void f(int i, int cw) { // 调用f(0, 0)
-        if (i >= 1){
-
-            arr.add(weight[i - 1]);
-        }
 
         if (cw == w || i == n) { // cw==w表示装满了，i==n表示物品都考察完了
             if (cw > maxW) {
                 maxW = cw;
             }
-
-            arr.clear();
+            System.out.println("目前的书包里有的物理："+arr.toString());
+            System.out.println();
             return;
         }
+
+
         f(i + 1, cw); // 选择不装第i个物品
+
         if (cw + weight[i] <= w) {
-            f(i + 1, cw + weight[i]); // 选择装第i个物品
+            arr.add(weight[i]);
+            f(i + 1, cw + weight[i]); // 尝试装第i个物品，尝试完成（迭代完成）把把拿出来进行下一次尝试
+            arr.remove(arr.size()-1);
         }
     }
-
 
     public static void main(String[] args) {
         new BackpackRe().f(0, 0);
